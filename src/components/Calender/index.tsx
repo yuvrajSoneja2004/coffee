9ed396cbe1b6cb79bd01c8200080cf9396ebdcf0
sairampatra <Yuvraj>
2024-05-14 15:15:14 +0530

    setup of sidebar

---
+++ src/components/Calender/index.tsx
@@ -0,0 +1,15 @@
+import Breadcrumb from "../Breadcrumbs/Breadcrumb";
+
+const DailyWorkData = () => {
+  return (
+    <div className="mx-auto max-w-7xl">
+      <Breadcrumb pageName="Daily Work Data" />
+
+      {/* <!-- ====== Calendar Section Start ====== --> */}
+
+      {/* <!-- ====== Calendar Section End ====== --> */}
+    </div>
+  );
+};
+
+export default DailyWorkData;
